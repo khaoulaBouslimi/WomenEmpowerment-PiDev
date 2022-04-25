d9568efa5d2b4c2147c5612426c9d9d737d90bd2
Khaoula <khaoula.bouslimi@esprit.tn>
2022-04-25 10:37:20 +0100

    i removed the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
+import { TrainingSessionListComponent } from './My-components/training-session-list/training-session-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateComponent
+    TemplateComponent,
+    TrainingSessionListComponent
   ],
   imports: [
     BrowserModule,
